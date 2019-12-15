806410ee73ad46d5028df911e8893179e9e48bcc
unknown <amierasyazlin97@gmail.com>
2019-12-16 03:03:17 +0700

    edit views on details

--- application/views/shop/details.php
+++ application/views/shop/details.php
@@ -1,33 +1,34 @@
 <div class="container-fluid">
+  <div class="row m-5">
 
-<div class="row text-center">
-<div class="card border-dark text-black bg-light mb-3 mt-3" style="max-width: 30rem;">
-  <h5 class="card-header">Product Details</h5>
+<div class="row text-center my-3">
+<div class="card border-dark text-black bg-light mb-3 mt-3" style="max-width: 100rem;">
+  <h2 class="card-header"><?= $barang['nama_barang']; ?></h2>
   <div class="card-body">
 
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-4">
         <img src="<?= base_url('assets/')?>img/<?= $barang['img_barang']?>.jpg" class="card-img">
       </div>
 
-      <div class="col-md-3"> </div>
+      <div class="col-md-8"> 
         <table class="table table-striped">
 
           <tr>
             <td>Product Name</td>
-            <td><strong><?= $barang['nama_barang']; ?></strong></td>
+            <td><strong><h3><?= $barang['nama_barang']; ?></strong></td>
           </tr>
 
           <td>Product Brand</td>
-            <td><strong><?= $barang['brand_barang']; ?></strong></td>
+            <td><strong><h3><?= $barang['brand_barang']; ?></strong></td>
           </tr>
 
            <td>Product Description</td>
-            <td><strong><?= $barang['deskripsi_barang']; ?></strong></td>
+            <td><strong><h3><?= $barang['deskripsi_barang']; ?></strong></td>
           </tr>
 
            <td>Product Price</td>
-            <td><strong><div class="btn btn-sm btn-success">Rp. <?= number_format($barang['harga_barang'], 0,',',',') ?></strong></td>
+            <td><strong><h3><div class="btn btn-lg btn-success">Rp. <?= number_format($barang['harga_barang'], 0,',',',') ?></strong></td>
           </tr>
         </table>
 
@@ -36,7 +37,8 @@
 
         <?php echo anchor ('shop/index/','<div class="btn btn-sm btn-light" id="right">Continue Shopping</div>') ?>
 
-        
+      </div>
+    </div>
       </div>
 
     </div>
