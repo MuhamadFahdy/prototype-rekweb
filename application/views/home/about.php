7b5ddb4bc59bc4f0baa08d2d06612fed40cbc949
unknown <amierasyazlin97@gmail.com>
2019-12-24 06:54:14 +0700

    edit about views

--- application/views/home/about.php
+++ application/views/home/about.php
@@ -1,5 +1,5 @@
 <div class="gambar2">
-  <img src="<?= base_url('assets/img/GB.jpg')?>" class="img-fluid" alt="Responsive image">
+  <img src="<?= base_url('assets/img/wpp.jpg')?>" class="img-fluid" alt="Responsive image">
 </div>
 
 <div class="container my-4">
@@ -25,7 +25,7 @@
       consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
       cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4><br>
-      <h2>Lorem Lorem</h2><br>
+      <h2>Life is hard enough already. Let us make it a little easier.</h2><br>
       <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua.</h4><br>
       <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -42,13 +42,8 @@
           <div class="container">
             <div class="row">
               <div class="col-sm-12 text-center">
-                <h1>Our Team</h1>
-                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h4>
+                <h1>TechBrain Team</h1>
+                <h4>Amazing Team consists of 5 members currently in Information Technology program from Universitas Pasundan. Purpose of this project is to implement all the knowledge obtained from Sir Sandhika Galih. </h4>
               </div>
             </div>
             </div>
@@ -58,54 +53,24 @@
                 <a href="" class="thumbnail">
                   <img src="<?= base_url('assets/img/logo2.png')?>">
                 </a>
-                <h4>My name</h4>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</h3>
+                <h4>TechBrain Returns and Cancellations</h4>
+                <h3>Products may be returned within 15 days of delivery for a refund of the purchase price and taxes, if returned with no damage and with all accessories, components and parts; however, some returns will be subject to a restocking fee.</h3>
               </div>
 
               <div class="col-sm-4">
                 <a href="" class="thumbnail">
                   <img src="<?= base_url('assets/img/logo2.png')?>">
                 </a>
-                <h4>My name</h4>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</h3>
+                <h4>TechBrain Download Center</h4>
+                <h3>Download the latest drivers, software, firmware and user manuals. Select the product series and model to find FAQs, drivers, manuals, warranty information and more.</h3>
               </div>
 
               <div class="col-sm-4">
                 <a href="" class="thumbnail">
                   <img src="<?= base_url('assets/img/logo2.png')?>">
                 </a>
-                <h4>My name</h4>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</h3>
-              </div>
-
-              <div class="col-sm-4">
-                <a href="" class="thumbnail">
-                  <img src="<?= base_url('assets/img/logo2.png')?>">
-                </a>
-                <h4>My name</h4>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</h3>
-              </div>
-
-              <div class="col-sm-4">
-                <a href="" class="thumbnail" >
-                  <img src="<?= base_url('assets/img/logo2.png')?>">
-                </a>
-                <h4>My name</h4>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</h3>
-              </div>
-
-              <div class="col-sm-4">
-                <a href="" class="thumbnail">
-                  <img src="<?= base_url('assets/img/logo2.png')?>">
-                </a>
-                <h4>My name</h4>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua.</h3>
+                <h4>TechBrain Warranty Info</h4>
+                <h3>Your warranty shall remain intact regardless if you perform the repair or seek repair services from a non-authorized third party.  If damages arise from such repair then TechBrain is not responsible for such damages resulting from the repair.</h3>
               </div>
 
             </div>
