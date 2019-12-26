2786ff32f4ee6c129505c2d4d3f297dfd23ab792
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-12-26 17:36:14 +0700

    Memperbaiki about

--- application/views/home/about.php
+++ application/views/home/about.php
@@ -1,86 +1,66 @@
-<div class="gambar2">
-  <img src="<?= base_url('assets/img/wpp.jpg')?>" class="img-fluid" alt="Responsive image">
-</div>
+<br>
+<br>
+<br>
+<br>
 
-<div class="container my-4">
-  <div class="row">
-    <div class="col">
-      <h1>ABOUT US</h1>
-    </div>
-    <div class="col">
-      <h4>TECH BRAIN is passionate about technology and driven by innovation. We dream, we dare and we strive to create an effortless and joyful digital life for everyone. We're always in search of incredible ideas and experiences, and we aspire to deliver the incredible in everything we do.</h4>
-      </div>
-  </div>
-</div>
 
-<div class="container my-4">
-  <div class="row">
-    <div class="col">
-      <h1>OUR STORY</h1>
-    </div>
-    <div class="col">
-      <h4>Discover the ideas, products and awards that make up our rich history as a global technology leader.</h4>
-      </div>
-  </div>
-</div>
+<div class="container">
+  
+    <div class="gtco-section gtco-testimonial gtco-gray">
+      <div class="gtco-container">
 
-<div class="container my-4">
-  <div class="row">
-    <div class="col">
-      <h1>OUR PEOPLE</h1>
-    </div>
-    <div class="col">
-      <h4>Meet the leaders, designers and entrepreneurs who guide and inspire our global business.</h4>
-      </div>
-  </div>
-</div>
+        <div class="row">
 
-<div class="container my-4">
-  <div class="row">
-    <div class="col">
-      <h1>OUR GOALS</h1>
-    </div>
-    <div class="col">
-      <h4>See how we’re helping to bridge the digital divide in communities around the world, and how we’re leading sustainable technology efforts.</h4>
-      </div>
-  </div>
-</div>
-
-  <section class="container-fruid text-center bg-secondary">
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-12 text-center">
-                <h1>Tech Brain Team</h1>
-                <h4>Amazing Team consists of 5 members currently in Information Technology program from Universitas Pasundan. Purpose of this project is to implement all the knowledge obtained from Sir Sandhika Galih. </h4>
+          <div class="col-12 animate-box">
+            <div class="gtco-testimony gtco-left">
+              <div class="text-center">
+                <h1>About us</h1>
+                <hr>
               </div>
             </div>
+          </div>
+
+          <div class="col-md-6 col-sm-6 animate-box">
+            <div class="gtco-testimony gtco-left">
+              <div><i class="fa fa-heart"></i> Mudah</div>
+              <blockquote>
+                <p>&ldquo;Website ini memberikan kemudahan dalam mencari sekaligus membeli barang elektronik.&rdquo; <cite class="author"></p>
+              </blockquote> 
             </div>
+          </div>
 
-            <div class="row">
-              <div class="col-sm-4">
-                <a href="" class="thumbnail">
-                  <img src="<?= base_url('assets/img/logo2.png')?>">
-                </a>
-                <h4>Tech Brain Returns and Cancellations</h4>
-                <h3>Products may be returned within 15 days of delivery for a refund of the purchase price and taxes, if returned with no damage and with all accessories, components and parts; however, some returns will be subject to a restocking fee.</h3>
-              </div>
-
-              <div class="col-sm-4">
-                <a href="" class="thumbnail">
-                  <img src="<?= base_url('assets/img/logo2.png')?>">
-                </a>
-                <h4>Tech Brain Download Center</h4>
-                <h3>Download the latest drivers, software, firmware and user manuals. Select the product series and model to find FAQs, drivers, manuals, warranty information and more.</h3>
-              </div>
-
-              <div class="col-sm-4">
-                <a href="" class="thumbnail">
-                  <img src="<?= base_url('assets/img/logo2.png')?>">
-                </a>
-                <h4>TechBrain Warranty Info</h4>
-                <h3>Your warranty shall remain intact regardless if you perform the repair or seek repair services from a non-authorized third party.  If damages arise from such repair then TechBrain is not responsible for such damages resulting from the repair.</h3>
-              </div>
-
+          <div class="col-md-6 col-sm-6 animate-box">
+            <div class="gtco-testimony gtco-left">
+              <div><i class="fa fa-heart"></i> Terpercaya</div>
+              <blockquote>
+                <p>&ldquo;Website ini sudah banyak yang menggunakan dan puas dengan hasil yang kami berikan &rdquo; <cite class="author"></p>
+              </blockquote> 
             </div>
-            
-  </section>
+          </div>
+
+          
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-sm-6 animate-box">
+            <div class="gtco-testimony gtco-left">
+              <div><i class="fa fa-heart"></i> Istimewa</div>
+              <blockquote>
+                <p>&ldquo;Website ini memberikan harga yang sangat terjangkau dan termurah.&rdquo; <cite class="author"></p>
+              </blockquote> 
+            </div>
+          </div>
+
+          <div class="col-md-6 col-sm-6 animate-box">
+            <div class="gtco-testimony gtco-left">
+              <div><i class="fa fa-heart"></i> Terjamin</div>
+              <blockquote>
+                <p>&ldquo;Website ini memberikan jaminan terhadap produk yang dijual.&rdquo; <cite class="author"></p>
+              </blockquote> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+</div>
