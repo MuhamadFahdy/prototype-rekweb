b6a41fe7a646ee8e0ec719dc9a9e4ebfe12293ed
unknown <amierasyazlin97@gmail.com>
2019-12-27 06:47:18 +0700

    edit about

--- application/views/home/about.php
+++ application/views/home/about.php
@@ -3,8 +3,6 @@
 <br>
 <br>
 
-
-<div class="container">
   
     <div class="gtco-section gtco-testimonial gtco-gray">
       <div class="gtco-container">
@@ -14,13 +12,16 @@
           <div class="col-12 animate-box">
             <div class="gtco-testimony gtco-left">
               <div class="text-center">
-                <h1>About us</h1>
+                <h1>About Us</h1>
+                <h4>TECH BRAIN is passionate about technology and driven by innovation. <br> We dream, we dare and we strive to create an effortless and joyful digital life for everyone. <br> We're always in search of incredible ideas and experiences, and we aspire to deliver the incredible in everything we do.</h4>
                 <hr>
               </div>
             </div>
           </div>
 
-          <div class="col-md-6 col-sm-6 animate-box">
+<div class="container text-center">
+  <div class="row">
+          <div class="col-md-3 col-sm-3 animate-box">
             <div class="gtco-testimony gtco-left">
               <div><i class="fa fa-heart"></i> Mudah</div>
               <blockquote>
@@ -29,7 +30,7 @@
             </div>
           </div>
 
-          <div class="col-md-6 col-sm-6 animate-box">
+          <div class="col-md-3 col-sm-3 animate-box">
             <div class="gtco-testimony gtco-left">
               <div><i class="fa fa-heart"></i> Terpercaya</div>
               <blockquote>
@@ -38,10 +39,7 @@
             </div>
           </div>
 
-          
-        </div>
-        <div class="row">
-          <div class="col-md-6 col-sm-6 animate-box">
+          <div class="col-md-3 col-sm-3 animate-box">
             <div class="gtco-testimony gtco-left">
               <div><i class="fa fa-heart"></i> Istimewa</div>
               <blockquote>
@@ -50,7 +48,7 @@
             </div>
           </div>
 
-          <div class="col-md-6 col-sm-6 animate-box">
+          <div class="col-md-3 col-sm-3 animate-box">
             <div class="gtco-testimony gtco-left">
               <div><i class="fa fa-heart"></i> Terjamin</div>
               <blockquote>
@@ -61,6 +59,33 @@
         </div>
       </div>
     </div>
-    
+  </div>
 
-</div>
+<section class="container text-center bg-dark">
+    <div class="row">
+              <div class="col-sm-4">
+                <a href="" class="thumbnail">
+                  <img src="<?= base_url('assets/img/package.png')?>" alt="logo" width="100" height="100">
+                </a>
+                <h4><strong>Tech Brain Returns and Cancellations</strong></h4>
+                <h5>Products may be returned within 15 days of delivery for a refund of the purchase price and taxes, <br> if returned with no damage and with all accessories, components and parts; <br> however, some returns will be subject to a restocking fee.</h5>
+              </div>
+
+              <div class="col-sm-4">
+                <a href="" class="thumbnail">
+                  <img src="<?= base_url('assets/img/file.png')?>" alt="logo" width="100" height="100">
+                </a>
+                <h4><strong>Tech Brain Download Center</strong></h4>
+                <h5>Download the latest drivers, software, firmware and user manuals. <br> Select the product series and model to find FAQs, drivers, manuals, warranty information and more.</h5>
+              </div> 
+
+              <div class="col-sm-4">
+                <a href="" class="thumbnail">
+                  <img src="<?= base_url('assets/img/warranty.png')?>" alt="logo" width="80" height="80">
+                </a>
+                <h4><strong>TechBrain Warranty Info</strong></h4>
+                <h5>Your warranty shall remain intact regardless if you perform the repair or seek repair services from a non-authorized third party. <br> If damages arise from such repair then TechBrain is not responsible for such damages resulting from the repair.</h5>
+              </div>
+            </div>
+    
+</section> 
